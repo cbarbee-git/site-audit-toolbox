c5641124de01677b9c36d06c67d7bbb6a09caf0e
Chad Barbee <chad_barbee@msn.com>
2023-12-10 11:56:36 -0500

    fix: add timeout check and ignore iframe loads

--- fetch.php
+++ fetch.php
@@ -1,5 +1,7 @@
 <?php
 require_once(rtrim($_SERVER['DOCUMENT_ROOT'], '/') . '/wp-load.php');
+use HeadlessChromium\BrowserFactory;
+use HeadlessChromium\Page;
 
 if (!isset($_GET['site-id']) || !is_numeric($_GET['site-id'])) {
     die('invalid site-id error.');
@@ -43,7 +45,7 @@ foreach($needed_directories as $key => $directory){
 
 /**TODO: Add a conditional for WP check */
 //if(IS_WORDPRESS()){
-$api_endpoint = "/wp-json/wp/v2/pages";
+$api_endpoint = "/wp-json/wp/v2/pages/";
 
 foreach($results as $result) {
     //get the curl url needed - with protocol and www
@@ -52,18 +54,32 @@ foreach($results as $result) {
     $full_site_url = $prepend_url . $result['site_url'];
     $response = call_curl($request_url);
     if($response === NULL){
-        die("Unable to reach API Endpoint. Is Site Wordpress?");
+        //don't die just yet...
+        //die("Unable to reach API Endpoint. Is Site Wordpress?");
+        //likely NOT a Wordpress site or malformed JSON, but grab the homepage at least
+        $response = array(array('link' => $full_site_url ));
     }
-    //loop through each page
-    foreach($response as $page){
-       $file_name = str_replace(array("https://".$result['site_url'], "https://www.".$result['site_url']), array('','') , $page['link']);
-        //clean up names -                        homepage            remove extra slashes
-        $file_name = ($file_name == '/') ? $result['site_url'] : str_replace('/','_',ltrim(rtrim($file_name, '/'),'/'));
-        ScreenShot($page['link'],$file_name . '.png',$needed_directories['temp_dir'],$viewport_array);
+    if(is_array($response)){
+        //loop through each page
+        foreach($response as $page){
+        $file_name = str_replace(array("https://".$result['site_url'], "https://www.".$result['site_url']), array('','') , $page['link']);
+            //iframe loading breaks (timeout?, chrome browser security - ignore these for now)
+            if(!str_contains($page['content']['rendered'],'<iframe')){
+                //clean up names -                        homepage            remove extra slashes
+                $file_name = ($file_name == '/') ? $result['site_url'] : str_replace('/','_',ltrim(rtrim($file_name, '/'),'/'));
+                ScreenShot($page['link'],$file_name . '.png',$needed_directories['temp_dir'],$viewport_array);
+            }
+        }
+        ZipScreenShots($result['site_url'],$needed_directories['output_dir'],$needed_directories['temp_dir']);
+        RemoveDirectory($needed_directories['temp_dir']);
+        echo('<p>' . $result['site_url'] . ' is done. ('.current_datetime()->format('Y-m-d H:i:sa').')</p>');
+    }else{
+        //this should never run. $response will always be an array of at least the homepage
+        echo('<p>Response was not valid.</p>');
+        echo('<pre>');
+        var_dump($response);
+        echo('</pre>');
     }
-    ZipScreenShots($result['site_url'],$needed_directories['output_dir'],$needed_directories['temp_dir']);
-    RemoveDirectory($needed_directories['temp_dir']);
-    echo('<p>' . $result['site_url'] . ' is done. ('.current_datetime()->format('Y-m-d H:i:sa').')</p>');
 }
 //}//if WORDPRESSS
 
@@ -81,18 +97,49 @@ function call_curl($request_url) {
     $response = curl_exec($curl);
 
     curl_close($curl);
-
-    return json_decode($response,TRUE);
+    //clean up any erronious info inside the response
+    $response = strstr($response, "[{" );
+    
+    $return = json_decode($response,TRUE);
+    $error = json_last_error();
+    //echo json response to be sure call returns correctly formatted json
+    echo("<p>JSON : ");
+    switch (json_last_error()) {
+        case JSON_ERROR_NONE:
+          echo "No errors";
+          break;
+        case JSON_ERROR_DEPTH:
+          echo "Maximum stack depth exceeded";
+          break;
+        case JSON_ERROR_STATE_MISMATCH:
+          echo "Invalid or malformed JSON";
+          break;
+        case JSON_ERROR_CTRL_CHAR:
+          echo "Control character error";
+          break;
+        case JSON_ERROR_SYNTAX:
+          echo "Syntax error";
+          break;
+        case JSON_ERROR_UTF8:
+          echo "Malformed UTF-8 characters";
+          break;
+        default:
+          echo "Unknown error";
+          break;
+    }
+      echo("</p>");
+    return $return;
 }
 
 function ScreenShot($urlToCapture,$file_name,$temp_file_path = 'temp',$viewport_array){
     require_once(__DIR__ . "/vendor/autoload.php");
+    //use HeadlessChromium\Page;
     $browser = (new HeadlessChromium\BrowserFactory()) -> createBrowser();
     foreach ($viewport_array as $label => $viewport){
         try {
             $page = $browser -> createPage();
             $page -> setViewport($viewport['width'],$viewport['height']);
-            $page -> navigate($urlToCapture) -> waitForNavigation();
+            $page -> navigate($urlToCapture) -> waitForNavigation(Page::DOM_CONTENT_LOADED, 120000);
             $page -> screenshot() -> saveToFile($temp_file_path.'/'.$label.'/'.$file_name);
         
         }
@@ -102,7 +149,7 @@ function ScreenShot($urlToCapture,$file_name,$temp_file_path = 'temp',$viewport_
             die("<p>ERROR !!</p>");
         }
         //finally {
-        //    $browser -> close();
+            //$browser -> close();
         //}
     }
     $browser -> close();
